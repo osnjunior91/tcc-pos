83827b5dcae0b1e2425821c0f4920c2ad310c9a2
Oswaldo Junior <osnjunior91@gmail.com>
2021-08-27 18:31:56 -0300

    Criação das entidades

---
+++ BoaEntrega/Customer.Api/Model/CustomerCreateRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Customer.Model
+{
+    public class CustomerCreateRequest
+    {
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public Address Address { get; set; }
+    }
+}
---
+++ BoaEntrega/Customer.Api/Model/CustomerResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customer.Model
+{
+    public class CustomerResponse
+    {
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public Address Address { get; set; }
+    }
+}
