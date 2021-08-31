169d87e427b135d9fdf1276f15d40a32bd9e9349
Oswaldo Junior <osnjunior91@gmail.com>
2021-08-31 17:57:55 -0300

    Criação do projeto de depositos.

--- BoaEntrega/Warehouse.Api/Controllers/ValuesController.cs
+++ BoaEntrega/Warehouse.Api/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Product.Controllers
+namespace Warehouse.Api.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : ControllerBase
