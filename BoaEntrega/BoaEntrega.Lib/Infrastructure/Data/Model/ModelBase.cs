48863551ec3e2eabc1bb698fb625b2a7dc8d6a72
Oswaldo Junior <osnjunior91@gmail.com>
2021-09-25 19:49:33 -0300

    Ajuste no acesso da api

--- BoaEntrega/BoaEntrega.Lib/Infrastructure/Data/Model/ModelBase.cs
+++ BoaEntrega/BoaEntrega.Lib/Infrastructure/Data/Model/ModelBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,7 @@ namespace BoaEntrega.Lib.Infrastructure.Data.Model
 {
     public class ModelBase
     {
+        [DynamoDBHashKey]
         public Guid Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
