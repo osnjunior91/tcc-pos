43b7cd6329ed0c544b73bca7a36487d56955362e
Oswaldo Junior <osnjunior91@gmail.com>
2021-10-15 15:11:40 -0300

    remove

--- BoaEntrega/Vehicle.Lib/Services/IVehicleService.cs
+++
@@ -1,9 +0,0 @@
-﻿using BoaEntrega.Lib.Service;
-using Vehicle.Lib.Infrastructure.Data;
-
-namespace Vehicle.Lib.Services
-{
-    public interface IVehicleService : IService<VehicleModel>
-    {
-    }
-}
